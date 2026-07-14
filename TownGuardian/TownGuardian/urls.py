fec58fe0f09cf4bae341373e44ca15599301a144
Stefan Gretcan <stefangretcan18@gmail.com>
2026-07-14 11:14:25 +0300

    test

--- TownGuardian/TownGuardian/urls.py
+++ TownGuardian/TownGuardian/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path, re_path,include
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
@@ -26,4 +29,6 @@ urlpatterns = [
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     re_path(r'^auth/', include('djoser.urls.jwt')),
+path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
